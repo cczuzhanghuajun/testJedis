04c24455be19ead284d79875a2b85828e39a1ad7
cczuzhanghuajun <zhang.huajun@cczu.edu.cn>
2021-09-18 13:41:44 +0800

    test

---
+++ testJedis/src/test/java/testJedis/testJedis/TestJedis.java
@@ -0,0 +1,14 @@
+package testJedis.testJedis;
+
+import redis.clients.jedis.Jedis;
+
+public class TestJedis {
+
+	public static void main(String[] args) {
+		Jedis jedis = new Jedis("localhost");
+		jedis.set("foo", "lala");
+		String value = jedis.get("foo");
+		System.out.print(value);
+	}
+
+}
